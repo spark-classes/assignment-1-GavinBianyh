26593381bce406b14b644fb83acb1bb763bf7bb1
Yuhe <gavinbianyuhe@hotmail.com>
2023-11-01 03:14:22 -0400

    Update QueueProcessor.cs

--- C_Sharp/API2/QueueProcessor.cs
+++ C_Sharp/API2/QueueProcessor.cs
@@ -1,41 +1,21 @@
-using System;
-using System.Threading.Tasks;
-using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
+using Azure.Identity;
 
 namespace API2
 {
     public class QueueProcessor
     {
+        string storageConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+
+
         [FunctionName("QueueProcessor")]
-        public void Run([QueueTrigger("api1queue", Connection = "StorageQueueUrl")]string myQueueItem, ILogger log)
+        public void Run([QueueTrigger("api1queue", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
-        }
-
-        [FunctionName("GetThirdSecret")]
-        public async Task<IActionResult> GetThirdSecret(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "secrets/secret3")] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("Fetching secret3 from Key Vault.");
-            string secretValue = await GetSecretFromKeyVaultAsync();
-            return new OkObjectResult(secretValue);
-        }
-
-        private async Task<string> GetSecretFromKeyVaultAsync()
-        {
-            var kvUri = "https://keybyh.vault.azure.net";
             var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
-
-            KeyVaultSecret secret = await client.GetSecretAsync("secret3");
-
-            return secret.Value;
+            KeyVaultSecret secret3 = client.GetSecret("secret3");
+            log.LogInformation($"Third secret's value: {secret3.Value}");
+            
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
     }
 }
