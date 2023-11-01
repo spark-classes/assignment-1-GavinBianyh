66f0f5d0b51e6be1591cc1fe46870c535cb1d4fa
Yuhe <gavinbianyuhe@hotmail.com>
2023-11-01 17:27:44 -0400

    Update QueueProcessor.cs

--- C_Sharp/API2/QueueProcessor.cs
+++ C_Sharp/API2/QueueProcessor.cs
@@ -8,7 +8,7 @@ namespace API2
     public class QueueProcessor
     {
         [FunctionName("QueueProcessor")]
-        public void Run([QueueTrigger("api1queue-poison", Connection = "newsetting")]string myQueueItem, ILogger log)
+        public void Run([QueueTrigger("api1queue", Connection = "newsetting")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
