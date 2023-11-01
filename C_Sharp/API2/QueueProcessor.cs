a8f7c784780fa1f5ddb3a36c8451c413455f5235
Yuhe <gavinbianyuhe@hotmail.com>
2023-11-01 16:58:57 -0400

    Update QueueProcessor.cs

--- C_Sharp/API2/QueueProcessor.cs
+++ C_Sharp/API2/QueueProcessor.cs
@@ -1,15 +1,15 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
 namespace API2
 {
     public class QueueProcessor
     {
-        string storageConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
-
-
         [FunctionName("QueueProcessor")]
-        public void Run([QueueTrigger("api1queue", Connection = "newsetting")]string myQueueItem, ILogger log)
+        public void Run([QueueTrigger("myqueue-items", Connection = "newsetting")]string myQueueItem, ILogger log)
         {
-            //log.LogInformation($"Third secret's value: {secret3.Value}");
-            
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
     }
