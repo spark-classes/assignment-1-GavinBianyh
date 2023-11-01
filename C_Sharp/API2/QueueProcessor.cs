b154530c0d07b0b392d6e6e25be9d255f3be4f2f
Yuhe <gavinbianyuhe@hotmail.com>
2023-11-01 16:54:13 -0400

    Update QueueProcessor.cs

--- C_Sharp/API2/QueueProcessor.cs
+++ C_Sharp/API2/QueueProcessor.cs
@@ -1,6 +1,3 @@
-using Azure.Security.KeyVault.Secrets;
-using Azure.Identity;
-
 namespace API2
 {
     public class QueueProcessor
@@ -11,9 +8,7 @@ namespace API2
         [FunctionName("QueueProcessor")]
         public void Run([QueueTrigger("api1queue", Connection = "newsetting")]string myQueueItem, ILogger log)
         {
-            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
-            KeyVaultSecret secret3 = client.GetSecret("secret3");
-            log.LogInformation($"Third secret's value: {secret3.Value}");
+            //log.LogInformation($"Third secret's value: {secret3.Value}");
             
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
