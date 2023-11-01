cf6d992c4e2968a4af98e8bd89771d4c78ff85da
Yuhe <gavinbianyuhe@hotmail.com>
2023-10-31 23:26:31 -0400

    Update test1.cs

--- C_Sharp/API1/test1.cs
+++ C_Sharp/API1/test1.cs
@@ -5,8 +5,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.Azure.Services.AppAuthentication;
-using Microsoft.Azure.KeyVault;
 using Azure.Security.KeyVault.Secrets;
 using Azure.Identity;
 
