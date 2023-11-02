76a6179b29c5e41380a48bffbab7fc87ae56af87
Yuhe <gavinbianyuhe@hotmail.com>
2023-11-01 21:36:21 -0400

    Update QueueProcessor.cs

--- C_Sharp/API2/QueueProcessor.cs
+++ C_Sharp/API2/QueueProcessor.cs
@@ -7,10 +7,22 @@ namespace API2
 {
     public class QueueProcessor
     {
+        private readonly SecretClient _secretClient;
+
+        public QueueProcessor()
+        {
+            var kvUri = "https://keybyh.vault.azure.net"; 
+            _secretClient = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+        }
+
         [FunctionName("QueueProcessor")]
         public void Run([QueueTrigger("api1queue", Connection = "newsetting")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+
+            // Read the third secret from Azure Key Vault
+            KeyVaultSecret thirdSecret = _secretClient.GetSecret("secret3");
+            log.LogInformation($"Third Secret Value: {secret3.Value}");
         }
     }
 }
